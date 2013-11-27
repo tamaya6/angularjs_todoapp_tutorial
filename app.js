20584cbd25ad82bae069be6fc5dfad595392bb54
Tatsunori Sunasaka <tamaya6@gmail.com>
2013-11-27 11:36:44 +0900

    新しいtodoを作成して表示する

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-angular.module('App', []);
+angular.module('App', [])
+.controller('MainController', ['$scope', function ($scope) {
+    $scope.todos = [];
+
+    $scope.addTodo = function () {
+        $scope.todos.push({
+            title: Math.random(),
+            done: false
+        });
+    };
+
+}]);
