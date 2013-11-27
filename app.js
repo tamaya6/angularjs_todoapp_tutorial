9652367e1f190c8d829aa1f62ebe8b8d2f8b0bce
tamaya6 <tamaya6@gmail.com>
2013-11-27 14:33:14 +0900

    未了・完了の件数を表示

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 angular.module('App', [])
-.controller('MainController', ['$scope', function ($scope) {
+.controller('MainController', ['$scope', '$filter', function ($scope, $filter) {
     $scope.todos = [];
     $scope.newTitle = '';
 
@@ -11,5 +11,29 @@ angular.module('App', [])
 
         $scope.newTitle = '';
     };
+
+    // フィルタリングの条件モデル
+    $scope.filter = {
+        done: { done: true },
+        remaining: {done: false }
+    };
+
+    // 現在のフィルタの状態モデル
+    $scope.currentFilter = null;
+
+    // フィルタリング条件を変更するメソッド
+    $scope.changeFilter = function (filter) {
+        $scope.currentFilter = filter;
+    };
+
+    var where = $filter('filter');    // filter関数の取得
+    $scope.$watch('todos', function (todos) {
+        // todosが増減したり各要素のプロパティが変更された時に実行される
+        var length = todos.length;
+
+        $scope.allCount = length;
+        $scope.doneCount = where(todos, $scope.filter.done).length;
+        $scope.remainingCount = length - $scope.doneCount;
+    }, true);
 
 }]);
